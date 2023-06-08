a51508f155789c944fe9bca8fae3aae632e24194
Sridhar <sridharmoturu@gmail.com>
2023-06-08 19:17:46 +0530

    few changes

--- src/Pages/Home/Categories.jsx
+++ src/Pages/Home/Categories.jsx
@@ -1,13 +1,19 @@
-import {catagoriesData } from "../../Data.jsx";
+import {categoriesData } from "../../Data.jsx";
 import CategoryItems from "./CategoryItems.jsx"
+import "../../Css/CategoriesItems.css";
+
 
 function Categories() {
   return (
-    <div>
-        {catagoriesData.map((item)=>{
-            <CategoryItems />
+    <>
+      <h3 className="heading">Choose your style</h3>
+      <div className="catge-con">
+        {categoriesData.map((item)=>{
+          <CategoryItems data={item}/>
+          console.log("this is coming from catagories")
         })}
-    </div>
+      </div>
+    </>
   )
 }
 
--- src/Pages/Home/CategoryItems.jsx
+++ src/Pages/Home/CategoryItems.jsx
@@ -1,9 +1,11 @@
-function CategoryItems() {
+import "../../Css/CategoriesItems.css";
+
+function CategoryItems({img,title}) {
   return (
-    <div className="cat-container">
-        <img src="" alt="" className="cat-img"/>
+     <div className="cat-container">
+        <img src={img} alt="" className="cat-img"/>
         <div className="cat-info">
-            <h3 className="cat-title"></h3>
+            <h3 className="cat-title">{title}</h3>
             <button>Shop Now</button>
         </div>
     </div>
